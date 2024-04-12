de7a83785589c3bf6fbabad280010b92f15a5095
Sonu-Shettiyar <sonushettiyar9393@gmail.com>
2024-04-12 16:08:56 +0530

    connection-corrected

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const formDataRouter = require('./routes/form.route');
 require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URL);
-
-const connection = mongoose.connection;
-connection.once('open', () => {
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('MongoDB database connection established successfully');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+    });
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
 });
 
 app.use('/', formDataRouter);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
